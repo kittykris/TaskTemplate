bdfdc57357cc01cc5028f5f6ee12ca13020ed05b
Kristina <kitty_kris@bk.ru>
2021-12-12 23:46:13 +0300

    fill class util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "12345";
+
+
+    public Connection getJDBCConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(MYSQL_DRIVER);
+            connection = DriverManager.getConnection(MYSQL_URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Problem with connection");
+        }
+        return connection;
+    }
 }
