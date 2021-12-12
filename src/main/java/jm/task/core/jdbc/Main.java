5345080205c68b8b7f119ed51c4c198342f6ee00
Kristina <kitty_kris@bk.ru>
2021-12-12 23:55:18 +0300

    fill class Main and run tests

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Maria", "Ivanova", (byte)23);
+        userService.saveUser("Olga", "Koroleva", (byte)23);
+        userService.saveUser("Ivan", "Kirillov", (byte)23);
+        userService.saveUser("Kirill", "Sapozhnikov", (byte)23);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
