26ecd50171097ad1e98099aba98224523019cbba
Kristina <kitty_kris@bk.ru>
2021-12-17 04:14:34 +0300

    do one connection on all class

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,60 +2,55 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
+    private final Connection connection = Util.getJDBCConnection();
+
     public UserDaoJDBCImpl() {
         //must be empty
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "CREATE TABLE IF NOT EXISTS users(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
-                    "name VARCHAR(30) NOT NULL, lastName VARCHAR(30) NOT NULL, age INT(3) NOT NULL);";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
+        String sql = "CREATE TABLE IF NOT EXISTS users(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
+                "name VARCHAR(30) NOT NULL, lastName VARCHAR(30) NOT NULL, age INT(3) NOT NULL);";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("Can't create table.");
         }
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "DROP TABLE IF EXISTS users;";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
+        String sql = "DROP TABLE IF EXISTS users;";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("Can't drop table.");
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, lastName);
-                preparedStatement.setInt(3, age);
-                preparedStatement.executeUpdate();
-            }
+        String sql = "INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Can't insert User into table.");
         }
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "DELETE FROM users WHERE id = ?;";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setLong(1, id);
-                preparedStatement.executeUpdate();
-            }
+        String sql = "DELETE FROM users WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("There is no such ID.");
         }
@@ -63,18 +58,16 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "SELECT * FROM users";
-            try (Statement statement = connection.createStatement()) {
-                ResultSet result = statement.executeQuery(sql);
-                while (result.next()) {
-                    User user = new User();
-                    user.setId(result.getLong(1));
-                    user.setName(result.getString(2));
-                    user.setLastName(result.getString(3));
-                    user.setAge(result.getByte(4));
-                    list.add(user);
-                }
+        String sql = "SELECT * FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet result = preparedStatement.executeQuery();
+            while (result.next()) {
+                User user = new User();
+                user.setId(result.getLong(1));
+                user.setName(result.getString(2));
+                user.setLastName(result.getString(3));
+                user.setAge(result.getByte(4));
+                list.add(user);
             }
         } catch (SQLException e) {
             System.out.println("There is no table.");
@@ -83,11 +76,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getJDBCConnection()) {
-            String sql = "DELETE FROM users";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
+        String sql = "DELETE FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Can't clean table.");
         }
